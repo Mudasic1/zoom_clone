3611d21524cd41c31c8dda39f99de837925d1c4b
Mudasir c <chandiommudasir084@gmail.com>
2025-01-27 14:00:33 +0500

    updated

---
+++ components/MobileNav.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { IoIosMenu } from "react-icons/io";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import link from "next/link";
+
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <IoIosMenu className="size-7 text-white cursor-pointer sm:hidden" />
+        </SheetTrigger>
+        <SheetContent side={"left"} className="border-none bg-dark-1">
+          <Link href={"/"} className="flex items-center gap-1">
+            <Image
+              src={"/icons/logo.svg"}
+              width={32}
+              height={32}
+              alt="Yoom logo"
+            
+            />
+            <p className="text-[26px] text-white font-bold">Yoom</p>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+
+          
+          <SheetClose asChild>
+            <section className="flex h-full flex-col gap-6 pt-16 text-white">
+            {sidebarLinks.map((link)=>{
+          const isActive = pathname === link.route;
+
+          return(
+            <Link href={link.route}
+            key={link.label}
+            className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+              'bg-blue-1': isActive
+            })}>
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p className='text-[15px] font-semibold '>
+                {link.label}
+              </p>
+            </Link>
+          )
+        })}
+            </section>
+
+          </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
